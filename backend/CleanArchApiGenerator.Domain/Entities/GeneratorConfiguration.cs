e9b1b096e46c81fd78cace0d24d219bdf6f83025
Bibek Chaudhary <bibek.chaudhary@avishkaram.com>
2026-02-18 14:23:52 +0545

    feat: add domain layer with generator configuration entity

---
+++ backend/CleanArchApiGenerator.Domain/Entities/GeneratorConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchApiGenerator.Domain.Entities
+{
+    public class GeneratorConfiguration
+    {
+        public string ProjectName { get; set; } = string.Empty;
+        public bool IncludeIdentity { get; set; }
+        public bool IncludeJwt { get; set; }
+        public bool IncludeSqlServer { get; set; }
+    }
+}
