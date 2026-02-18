d2a115ec7b5eada02c61644e810d473b30796703
Bibek Chaudhary <bibek.chaudhary@avishkaram.com>
2026-02-18 14:35:30 +0545

    feat: implement dotnet cli infrastructure service

---
+++ backend/CleanArchApiGenerator.Infrastructure/Services/DotnetCliService .cs
@@ -0,0 +1,38 @@
+﻿using CleanArchApiGenerator.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchApiGenerator.Infrastructure.Services
+{
+    public class DotnetCliService : IDotnetCliService
+    {
+        public async Task RunAsync(string workingDirectory, string arguments) {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "dotnet",
+                    Arguments = arguments,
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                }
+            };
+
+            process.Start();
+
+            await process.WaitForExitAsync();
+
+            if(process.ExitCode != 0)
+            {
+                var error = await process.StandardError.ReadToEndAsync();
+                throw new Exception($"dotnet CLI command failed with exit code {process.ExitCode}: {error}");
+            }
+        }
+    }
+}
