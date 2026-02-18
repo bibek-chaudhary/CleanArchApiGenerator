cefd2ab9953be4e2baa7653a54bd86d3b1fbfc45
Bibek Chaudhary <bibek.chaudhary@avishkaram.com>
2026-02-18 14:29:10 +0545

    feat: define application layer interfaces

---
+++ backend/CleanArchApiGenerator.Application/Interfaces/IProjectGeneratorService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchApiGenerator.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchApiGenerator.Application.Interfaces
+{
+    public interface IProjectGeneratorService
+    {
+        Task<string> GenerateAsync(GeneratorConfiguration config);
+    }
+}
---
+++ backend/CleanArchApiGenerator.Application/Interfaces/IZipService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchApiGenerator.Application.Interfaces
+{
+    public interface IZipService
+    {
+        string CreateZip(string sourcePath, string projectName);
+    }
+}
